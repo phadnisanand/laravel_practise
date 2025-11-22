b43b9b08376b15c9013d8e3e989480b2b74b17a1
Your Name <you@example.com>
2025-11-23 00:14:02 +0530

    laravel breeze demo

---
+++ breezedemo/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\http\Middleware\IsAdminMiddleware;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(middleware: ['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('categories', CategoryController::class)->middleware(IsAdminMiddleware::class);
+    Route::resource('posts', PostController::class)->middleware(IsAdminMiddleware::class);
+});
+
+
+
+require __DIR__.'/auth.php';
--- example-app/app/Http/Controllers/API/PostController.php
+++ example-app/app/Http/Controllers/API/PostController.php
@@ -8,7 +8,7 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Validator;
 class PostController extends Controller
 {
-    
+
     public function index(Request $request) {
         $posts = Post::paginate(5);
         return response()->json([
@@ -17,7 +17,7 @@ class PostController extends Controller
             "data" => $posts
         ]);
     }
-    public function update(Request $request, $id) { 
+    public function update(Request $request, $id) {
          $validator = Validator::make($request->all(), [
             "name"=> "required",
             'body' => "required"
@@ -30,23 +30,23 @@ class PostController extends Controller
             'data' => $validator->errors()->all()
          ]);
         }
-        
+
         $post = Post::find($id);
         $post->name = $request->name;
         $post->body = $request->body;
-        $post->save();  
+        $post->save();
         return response()->json([
             'status'=> 0,
             'message'=> 'Post updated',
             'data' => $post
         ]);
     }
-    
+
     public function destroy(Request $request, $id) {
         $post = Post::find($id);
         $post->delete();
-        return response()->json([   
-            "status"=> 0,  
+        return response()->json([
+            "status"=> 0,
             "message"=> "Post deleted"
         ]);
     }
@@ -65,7 +65,7 @@ class PostController extends Controller
             'data' => $validator->errors()->all()
          ]);
         }
-   
+
          $posts = Post::create(
             ['name'=> $request->name,'body'=> $request->body]
          );
